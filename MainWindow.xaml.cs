f5b995eb212f60d98f7fb715dbcd55f64dc75c5c
azizaskarow <azizasqarov773@gmail.com>
2023-11-17 12:24:29 +0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Scanner.UI.LoginFolder;
+
+namespace Scanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var loginWin = new LoginWin();
+            loginWin.Show();
+        }
+    }
+}
---
+++ UI/LoginFolder/LoginWin.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Scanner.UI.LoginFolder;
+
+/// <summary>
+/// Interaction logic for LoginWin.xaml
+/// </summary>
+public partial class LoginWin : Window
+{
+    public LoginWin()
+    {
+        InitializeComponent();
+    }
+
+    private void Window_Closed(object? sender, EventArgs e)
+    {
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+            this.DragMove();
+    }
+
+    private void ExitBtn_Click(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+
+    private void butt_Log_Click(object sender, RoutedEventArgs e)
+    {
+    }
+
+    private void text_passw_hide_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+    }
+}
